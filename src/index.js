1c495440bc100d4b736ed7419f0ce1af1e184792
Trushart-Jangade <trusharth.janghade@kpit.com>
2018-11-01 16:10:01 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -4,7 +4,59 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+function FancyBorder(props) {
+    return (
+      <div className={'FancyBorder FancyBorder-' + props.color}>
+        {props.children}
+      </div>
+    );
+  }
+  
+function Dialog(props) {
+    return (
+      <FancyBorder color="blue">
+        <h1 className="Dialog-title">
+          {props.title}
+        </h1>
+        <p className="Dialog-message">
+          {props.message}
+        </p>
+        {props.children}
+      </FancyBorder>
+    );
+  }
+  
+  class SignUpDialog extends React.Component {
+    constructor(props) {
+      super(props);
+      this.handleChange = this.handleChange.bind(this);
+      this.handleSignUp = this.handleSignUp.bind(this);
+      this.state = {login: ''};
+    }
+  
+    render() {
+      return (
+        <Dialog title="Mars Exploration Program"
+                message="How should we refer to you?">
+          <input value={this.state.login}
+                 onChange={this.handleChange} />
+  
+          <button onClick={this.handleSignUp}>
+            Sign Me Up!
+          </button>
+        </Dialog>
+      );
+    }
+  
+    handleChange(e) {
+      this.setState({login: e.target.value});
+    }
+  
+    handleSignUp() {
+      alert(`Welcome aboard, ${this.state.login}!`);
+    }
+  }
+ReactDOM.render(<SignUpDialog />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
